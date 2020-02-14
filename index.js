fa9d2fd10a107816bca2b27956296dbb02395039
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-14 15:27:45 -0500

    looking for a code review

--- index.js
+++ index.js
@@ -44,10 +44,130 @@ server.get("/api/projects/:id", (req, res) => {
     });
 });
 
-server.post("/api/projects/:id", (req, res) => {
-  Dbs.insert(req.body);
-  console.log(req.body);
+server.post("/api/projects", (req, res) => {
+  Dbs.insert(req.body)
+    .then(projects => {
+      if (!req.body.project_id) {
+        res.status(400).json({ errorMessage: "please provide project_id" });
+      } else {
+        res.status(201).json(projects);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "there was an error while saving the project to database "
+      });
+    });
 });
+
+server.post("/api/actions", (req, res) => {
+  Dbs.insert(req.body)
+    .then(action => {
+      if (!dbInfo.name || !dbInfo.bio) {
+        res.status(400).json({ errorMessage: "please provide project_id" });
+      } else {
+        res.status(201).json(action);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "there was an error while saving the project to database "
+      });
+    });
+});
+
+server.put("/api/projects/:id", (req, res) => {
+  const updateProject = req.params.id;
+  Dbs.update(updateProject, req.body)
+    .then(projects => {
+      if (projects.name && project.description) {
+        res.status(200).json(projects);
+      } else {
+        res
+          .status(400)
+          .json({ errorMassage: "make sure to add name and discription" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The user information could not be modified."
+      });
+    });
+});
+
+server.put("/api/actions/:id", (req, res) => {
+  const updateAction = req.params.id;
+  Dbs.update(updateAction, req.body)
+    .then(action => {
+      if (action.project_id && action.notes) {
+        res.status(200).json(projects);
+      } else {
+        res
+          .status(400)
+          .json({ errorMassage: "make sure to add project_id and notes" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The user information could not be modified."
+      });
+    });
+});
+
+server.delete("/api/projects/:id", (req, res) => {
+  Dbs.findById(req.params.id)
+    .then(project => {
+      if (project.length === 0) {
+        res.status(400).json({
+          message: " the project with the specified Id does not exit"
+        });
+      } else {
+        Dbs.remove(req.params.id)
+          .then(project => {
+            res
+              .status(200)
+              .json({ message: "The id has been succefully removed" });
+          })
+          .catch(error => {
+            res.status(500).json({ errorMessage: " failed to remove" });
+          });
+      }
+    })
+
+    .catch(error => {
+      res.status(500).json({ errorMessage: " post could not be removed" });
+    });
+});
+
+router.delete("/api/actions/:id", (req, res) => {
+  Dbs.findById(req.params.id)
+    .then(action => {
+      if (action.length === 0) {
+        res
+          .status(400)
+          .json({ message: " the action with the specified Id does not exit" });
+      } else {
+        Dbs.remove(req.params.id)
+          .then(action => {
+            res
+              .status(200)
+              .json({ message: "The id has been succefully removed" });
+          })
+          .catch(error => {
+            res.status(500).json({ errorMessage: " failed to remove" });
+          });
+      }
+    })
+
+    .catch(error => {
+      res.status(500).json({ errorMessage: " post could not be removed" });
+    });
+});
+
 const port = 5000;
 
 server.listen(port, () => console.log(`\n** API on port ${port} \n`));
